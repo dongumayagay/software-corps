d7bda72d8f868509c23fd54292317ad66b6e8c6f
Roselyn Mae Maling <roselynmaybe@gmail.com>
2022-11-16 00:19:14 +0800

    added contact page placeholder

---
+++ src/routes/(customer)/contact/+page.svelte
@@ -0,0 +1,7 @@
+
+<main class=" bg-zinc-500 grid place-items-center min-h-screen">
+	<div class=" flex w-full max-w-lg bg-orange-200">
+		<section class=" flex-1 bg-orange-400">Contact Us</section>
+		<section class=" flex-1 bg-pink-500">Form</section>
+	</div>
+</main>
